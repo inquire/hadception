57699c5e83c1d2b3fb58e36b5e01ee562fad121a
inquire <stanoescu.dan@gmail.com>
2012-02-08 23:47:10 +0000

    Adding a skeleton that will wrap all Nested R&W

---
+++ src/NestedUtils.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.compress.CompressionCodec;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.mapred.join.InnerJoinRecordReader;
+import org.apache.hadoop.mapreduce.*;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+//import org.apache.hadoop.io.RawComparator;
+//import org.apache.hadoop.io.compress.CompressionCodec;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ReflectionUtils;
+
+
+
+public class NestedUtils <KEY, VALUE, CONTEXT> {
+
+	
+	
+	
+	
+}
